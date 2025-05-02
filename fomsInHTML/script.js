bd17b1529e28e669b693cabf08b64a43591c9166
Raj Vardhan <rajvardaan130@gmail.com>
2025-05-02 13:52:17 +0530

    js added

---
+++ fomsInHTML/script.js
@@ -0,0 +1,8 @@
+function login() {
+  const unm = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+  const quali = document.getElementById("qualification").value;
+  const add = document.getElementById("address").value;
+
+  console.log(unm, pass, add, quali);
+}
